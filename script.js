bd3df9a8d3cf15d551d03f861095431faad2033f
LoganEagerUc <lea46@uclive.ac.nz>
2025-10-25 16:01:27 +1300

    feat: added icon and visual aid arrow logic to help with new window scaling

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+
+// SCROLL VISUAL AID FUNCTION, (downwards arrow icon behaviour)
+
+// references
+const footer = document.querySelector("footer");
+
+// local vars
+let iconVisible = true; // flag as to wether it *should* be visible
+let inactivityTimer = null;
+
+document.addEventListener("scroll", handleScroll, { passive: true });
+
+// initial state
+if (footer) {
+  footer.classList.add("fade-in");
+}
+
+// triggers every scroll event
+function handleScroll() {
+  iconVisible = false;
+  handleIconAnimation();
+
+  // resets timer
+  if (inactivityTimer) clearTimeout(inactivityTimer);
+  inactivityTimer = setTimeout(() => {
+    iconVisible = true;
+    handleIconAnimation();
+  }, 60000); // 60,000ms == 1 min
+}
+
+function handleIconAnimation() {
+  if (!footer) return;
+
+  if (iconVisible) {
+    footer.classList.remove("fade-out");
+    footer.classList.add("fade-in");
+  } else {
+    footer.classList.remove("fade-in");
+    footer.classList.add("fade-out");
+  }
+}
