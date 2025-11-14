20dba9b74d7882d038ed323d335f172112498a7f
Logan Eager <loganallaneager@gmail.com>
2025-11-14 15:17:28 +1300

    refactor: remove deprecated mobile device warning and smooth scroll scripts; update index.html to reflect changes

--- Scripts/mobile-device-warning.js
+++
@@ -1,9 +0,0 @@
-// temporary script till support.
-window.onload = function () {
-  if (isMobileDevice()) {
-    // Display a warning alert
-    alert(
-      "I havent added mobile support yet. Please visit on your computer, or wait till I add mobile support, thanks."
-    );
-  }
-};
